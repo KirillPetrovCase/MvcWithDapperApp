d0ee03e608d3c1faa5889f3fd8fbcd1eb134196b
KirillPetrovCase <kirill.petrov.work.email@gmail.com>
2021-06-24 13:05:54 +0300

    Add project files.

---
+++ Models/UserRepository.cs
@@ -0,0 +1,75 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DapperApp.Models
+{
+    public interface IRepository
+    {
+        void Create(User user);
+        void Delete(int id);
+        User Get(int id);
+        List<User> GetUsers();
+        void Update(User user);
+    }
+
+    public class UserRepository : IRepository
+    {
+        private readonly string _connectionString = null;
+
+        public UserRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Create(User user)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var query = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age)";
+                db.Execute(query, user);
+
+                // если мы хотим получить id добавленного пользователя
+                //var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age); SELECT CAST(SCOPE_IDENTITY() as int)";
+                //int? userId = db.Query<int>(sqlQuery, user).FirstOrDefault();
+                //user.Id = userId.Value;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var sqlQuery = "DELETE FROM Users WHERE Id = @id";
+                db.Execute(sqlQuery, new { id });
+            }
+        }
+
+        public User Get(int id)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                return db.Query<User>($"SELECT * FROM Users Where Id = @Id", new { id }).FirstOrDefault();
+            }
+        }
+
+        public List<User> GetUsers()
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                return db.Query<User>("SELECT * FROM Users").ToList();
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var sqlQuery = "UPDATE Users SET Name = @Name, Age = @Age WHERE Id = @Id";
+                db.Execute(sqlQuery, user);
+            }
+        }
+    }
+}
